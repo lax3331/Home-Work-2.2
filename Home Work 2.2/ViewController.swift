b61312bae39df15685e83743c0526d0556706dd8
Nikky <la2life1234@gmail.com>
2022-12-20 14:47:14 +0300

    Initial Commit

---
+++ Home Work 2.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Home Work 2.2
+//
+//  Created by Nikita Kuznetsov on 20.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
