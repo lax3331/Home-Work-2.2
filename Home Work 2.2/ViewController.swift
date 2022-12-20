6104723ab33b55c085aed429a49f4317c746c256
Nikky <la2life1234@gmail.com>
2022-12-20 15:00:36 +0300

    worked slider + color

--- Home Work 2.2/ViewController.swift
+++ Home Work 2.2/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var mainView: UIView!
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //MARK: View
+        
+        //MARK: labels
+        redLabel.textColor = .red
+        greenLabel.textColor = .green
+        blueLabel.textColor = .blue
+        //MARK: Sliders
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+        blueSlider.minimumTrackTintColor = .blue
     }
 
-
+    @IBAction func redSliderAction(_ sender: Any) {
+        redLabel.text = "Red: \(String(Int(redSlider.value)))"
+    }
+    
+    @IBAction func greenSliderAction(_ sender: Any) {
+        greenLabel.text = "Green: \(String(Int(greenSlider.value)))"
+    }
+    
+    @IBAction func blueSliderAction(_ sender: Any) {
+        blueLabel.text = "Blue: \(String(Int(blueSlider.value)))"
+    }
+    
+    
 }
 
